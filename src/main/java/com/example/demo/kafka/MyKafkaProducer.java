2c335eaa603bafd31cda99f738665ba1cb833023
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-18 23:52:34 +0530

    Added code for kafka

--- src/main/java/com/example/demo/kafka/MyKafkaProducer.java
+++ src/main/java/com/example/demo/kafka/MyKafkaProducer.java
@@ -1,6 +1,10 @@
 package com.example.demo.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 
 import java.util.Properties;
@@ -11,6 +15,10 @@ public class MyKafkaProducer {
 
     private static Producer<Long, String> createProducer(){
         Properties props = new Properties();
-        /*props.put-*/
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return new KafkaProducer<Long, String>(props);
     }
 }
