27764aee95ae39f5740c30cb6dec86346980d941
Kavish Rizvi <rizvikavish205.com>
2021-09-21 18:22:20 +0530

    consumer config

--- src/main/java/com/kavish/AppController.java
+++ src/main/java/com/kavish/AppController.java
@@ -1,6 +1,8 @@
-package com.example.demo;
+package com.kavish;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class AppController {
 
-	@RequestMapping("/")
-	String home() {
-		return "hello Coder";
+	@PostMapping("v1/write")
+	public String writeToMessageQueue(@RequestBody String payload){
+
+		return "";
 	}
 }
---
+++ src/main/java/com/kavish/kafka/MyKafkaProducer.java
@@ -0,0 +1,23 @@
+package com.kavish.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class MyKafkaProducer {
+    private final static String TOPIC = "users";
+    private final static String BOOTSTRAP_SERVERS = "192.168.1.1:9092,192.168.1.1:9093,192.168.1.1:9094";
+
+    private static Producer<Long, String> createProducer(){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return new KafkaProducer<Long, String>(props);
+    }
+}
