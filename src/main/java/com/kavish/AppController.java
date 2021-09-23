105b37ae9df44a9acc7d21ca38f4140ca10f788e
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-24 01:06:11 +0530

    added code

--- src/main/java/com/kavish/AppController.java
+++ src/main/java/com/kavish/AppController.java
@@ -1,18 +1,22 @@
 package com.kavish;
 
+import com.kavish.kafka.MyKafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class AppController {
 
-	@PostMapping("v1/write")
-	public String writeToMessageQueue(@RequestBody String payload){
+    @Autowired
+    private MyKafkaProducer producer;
 
-		return "";
-	}
+    @PostMapping("v1/write")
+    public String writeToMessageQueue(@RequestBody String payload) {
+        producer.sendMessage(payload);
+        return "Message Successfully Pushed";
+    }
 }
--- src/main/java/com/kavish/kafka/MyKafkaProducer.java
+++ src/main/java/com/kavish/kafka/MyKafkaProducer.java
@@ -3,7 +3,9 @@ package com.kavish.kafka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyKafkaProducer {
     @Value(value = "${users.topic.name}")
     private String usersTopicName;
