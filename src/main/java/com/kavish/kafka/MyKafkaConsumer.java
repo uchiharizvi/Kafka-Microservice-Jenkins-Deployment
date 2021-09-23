119d9b4999b84a804ee5f531b7776fcd236ac581
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-24 00:18:43 +0530

    Consumer Code

--- src/main/java/com/kavish/kafka/MyKafkaConsumer.java
+++ src/main/java/com/kavish/kafka/MyKafkaConsumer.java
@@ -6,19 +6,17 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 
 import java.util.Properties;
 
 public class MyKafkaConsumer {
-    private final static String TOPIC = "users";
-    private final static String BOOTSTRAP_SERVERS = "192.168.1.1:9092,192.168.1.1:9093,192.168.1.1:9094";
+    @Value(value = "${users.topic.name}")
+    private String usersTopicName;
 
-    private static Consumer<Long, String> createConsumer() {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return new KafkaConsumer<Long, String>(props);
+    @KafkaListener(topics = "dev-user", groupId = "")
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message " + message);
     }
 }
